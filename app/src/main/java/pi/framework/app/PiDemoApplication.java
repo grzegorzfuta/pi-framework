ff2f7806d8242150b608a54454be9445357f857b
Grzegorz Futa <grzegorz.futa@hpe.com>
2018-05-28 14:33:49 +0200

    STEP 04 implementacja serwisów i warstwy DAO
    - implementacja serwisów
    - implementacja DAO/persistensce
    - zmiana kodu aplikacji

--- app/src/main/java/pi/framework/app/PiDemoApplication.java
+++ app/src/main/java/pi/framework/app/PiDemoApplication.java
@@ -5,6 +5,8 @@ import pi.framework.app.service.ImportantBusinessService;
 import pi.framework.core.context.ApplicationContext;
 import pi.framework.core.context.PiApplicationContext;
 
+import java.util.List;
+
 public class PiDemoApplication {
 
     private PiDemoApplication(){}
@@ -16,9 +18,23 @@ public class PiDemoApplication {
 
         ImportantBusinessService someImportantService = applicationContext.getBean(ImportantBusinessService.class);
 
-        PersonDTO krzychoDTO = someImportantService.fetchImportantPerson("Krzycho");
+        PersonDTO randomImportantPerson = someImportantService.fetchRandomImportantPerson();
+        System.out.println("RANDOM IMPORTANT PERSON");
+        System.out.println(randomImportantPerson);
 
-        System.out.println(krzychoDTO);
+        PersonDTO personByID = someImportantService.fetchByID(4);
+        System.out.println("OSOBA PO NUMERZE ID");
+        System.out.println(personByID);
+
+        PersonDTO johnDoe = someImportantService.fetchByID(4);
+        System.out.println("NO-NAME");
+        System.out.println(johnDoe);
+
+        List<PersonDTO> marlins = someImportantService.fetchByName("Marlin");
+        System.out.println("MARLINS");
+        marlins.forEach(System.out::println);
+
+        System.out.println("Winter demo application... finished");
     }
 
 }
