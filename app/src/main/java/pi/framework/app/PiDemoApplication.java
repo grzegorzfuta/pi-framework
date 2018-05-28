b07ad8f24493bc5bd5b8afffc611c73e6647855d
dominisz <dominisz@gmail.com>
2018-05-28 10:40:14 +0200

    Initial commit - only main class

---
+++ app/src/main/java/pi/framework/app/PiDemoApplication.java
@@ -0,0 +1,11 @@
+package pi.framework.app;
+
+public class PiDemoApplication {
+
+    private PiDemoApplication(){}
+
+    public static void main(String[] args) {
+        System.out.println("PI framework demo application...");
+    }
+
+}
