352fbccf9293abe1366c599dc94ee4a1854d8d0d
Grzegorz Futa <grzegorz.futa@hpe.com>
2018-05-29 09:07:03 +0200

    STEP 06 (also important) - instancjonowanie komponentów
    - instancjonowanie komponentów
    - zmiany BeanDefinitionInfo

--- framework/src/main/java/pi/framework/core/context/PiApplicationContext.java
+++ framework/src/main/java/pi/framework/core/context/PiApplicationContext.java
@@ -3,31 +3,71 @@ package pi.framework.core.context;
 import pi.framework.core.bean.BeanDefinitionInfo;
 import pi.framework.core.scanner.ClassScanner;
 import pi.framework.core.scanner.ScannerFactory;
+import pi.framework.core.stereotype.Component;
+import pi.framework.core.stereotype.Repository;
+import pi.framework.core.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.lang.annotation.Annotation;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableCollection;
 import static pi.framework.core.scanner.ClassScanner.Scanner.ANNOTATION_SCANNER;
 
 public class PiApplicationContext implements ApplicationContext {
 
-    protected Set<BeanDefinitionInfo> beanDefinitions = new HashSet<>();
+    private final Collection<Class> VALID_ANNOTATION_CLASSES = unmodifiableCollection(asList(
+            Component.class, Repository.class, Service.class));
+
+    private final Set<BeanDefinitionInfo> beanDefinitions = new HashSet<>();
 
     public PiApplicationContext(Class clazz) {
         Package pckg = clazz.getPackage();
 
         ClassScanner classScanner = ScannerFactory.getScanner(ANNOTATION_SCANNER, pckg.getName());
-        Set<Class<?>> beanClasses = classScanner.getAllBeanClasses();
+        Set<Class<?>> piComponents = classScanner.getAllBeanClasses();
+        instantiateComponents(piComponents);
+    }
+
+    private void instantiateComponents(Set<Class<?>> piComponents) {
+        piComponents.forEach(componentClass -> {
+            String className = componentClass.getName();
+            try {
+                Class newClass = Class.forName(className);
+                BeanDefinitionInfo beanDefinition = createBeanInstance(newClass);
+                if (beanDefinition.getName() != null) {
+                    beanDefinitions.add(beanDefinition);
+                }
+            } catch (ClassNotFoundException exception) {
+                exception.printStackTrace();
+            }
+        });
+    }
+
+    private BeanDefinitionInfo createBeanInstance(Class aClass) {
+        BeanDefinitionInfo beanDefinition = new BeanDefinitionInfo();
+        try {
+            Object instance = aClass.newInstance();
+            beanDefinition.setClazz(aClass);
+            beanDefinition.setInstance(instance);
+            beanDefinition.setName(aClass.getName());
+        } catch (ReflectiveOperationException exception) {
+            exception.printStackTrace();
+        }
+        return beanDefinition;
     }
 
     @Override
     public <T> T getBean(Class<T> clazz) {
-        throw new UnsupportedOperationException("To be implemented in the next step");
+        // collection `beanDefinitions` is filled
+        throw new UnsupportedOperationException("To be implemented in the next step. The next one.");
     }
 
     @Override
     public <T> T getBean(String componentName) {
-        throw new UnsupportedOperationException("To be implemented in the next step");
+        throw new UnsupportedOperationException("To be implemented in the next step.  The next one.");
     }
 
 }
